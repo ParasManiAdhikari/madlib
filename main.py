a13b81bf4c73f93262df9c51c65d019736c98671
Paras Adhikari <parasad612@gmail.com>
2021-08-11 21:31:53 +0200

    starting out

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 youtuber = "Ktiboss"
 print("Subscribe to " + youtuber)
 print(f"Subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+famous = input("A famous person: ")
+verb1= input("A verb: ")
+verb2 = input("Another verb: ")
+
+madlib = f"Hey {adj} guy you have to stop comparing yourself to {famous} because you are \
+fine the way you are. You can {verb1}, you can {verb2} and you can also code!"
+
+print(madlib)
